1e44994f74f0255fc9f9a0a27889af64a51855ef
Bob Reselman <bob@CogArtTech.com>
2022-11-06 16:32:07 -0800

    simplifying output

--- runforever/runforever.c
+++ runforever/runforever.c
@@ -8,23 +8,8 @@
 #endif
 
 int main(int argc, char *argv[]) {
-    int n1 = 1;
-
-    char *num;
-    char buffer[MAX];
     while( 1 == 1){
-        if(n1==100000){
-            printf("The value n has reached the limit of 100000. Exiting.");
-            exit(EXIT_SUCCESS);
-        }
-        if (asprintf(&num, "%d", n1) == -1) {
-        perror("asprintf");
-        } else {
-            strcat(strcpy(buffer, "HELLO WORLD! run number="), num);
-            printf("%s\n", buffer);
-            free(num);
-            n1++;
-        } 
+        printf(".");
     }
     exit(EXIT_SUCCESS);
 }
