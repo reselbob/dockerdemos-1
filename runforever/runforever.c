5b80b27acbdff58a55f3f532ba87606cf806ac7e
Bob Reselman <bob@CogArtTech.com>
2022-11-06 16:39:48 -0800

    simplifying output

--- runforever/runforever.c
+++ runforever/runforever.c
@@ -7,8 +7,24 @@
 #define MAX 100
 #endif
 
+#include <time.h>
+ 
+void delay(int number_of_half_seconds)
+{
+    // Converting time into milli_seconds
+    int milli_seconds = 500 * number_of_half_seconds;
+ 
+    // Storing start time
+    clock_t start_time = clock();
+ 
+    // looping till required time is not achieved
+    while (clock() < start_time + milli_seconds)
+        ;
+}
+
 int main(int argc, char *argv[]) {
     while( 1 == 1){
+        delay(1);
         printf(".");
     }
     exit(EXIT_SUCCESS);
